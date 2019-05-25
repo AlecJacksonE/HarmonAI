bfea612d8bfc43e3f90fdd152a47dfdd9c9230af
AlecJacksonE <alecej@uci.edu>
2019-05-24 20:28:28 -0700

    Changed implementation to include 2 agents. Unfortunately this can only be hardcoded i think, since for each agent we need to update the xml for the world. I also had to hardcode the attack action for each agent so they apear one after another in the code. Otherwise the second agent lags behind the first one.

--- code/musician.py
+++ code/musician.py
@@ -10,41 +10,28 @@ class Musician():
 
 	def __init__(self, note_positions):
 		self.note_positions	= note_positions
-	#Teleports the agent to location (x, 227, z) on a good_frame.
-	def teleport(self, agent_host, x, z):
-		#Construct and execute the teleport command.
-		tp_command = "tp " + str(x + 0.5) + " 227 " + str(z-1)
-		agent_host.sendCommand(tp_command)
+		self.in_position = False
+		self.can_play = False
 
-		#Ensures that we leave on a good frame.
-		good_frame = False 
-		start = timer()
-		while not good_frame:
-			world_state = agent_host.getWorldState()
-			if not world_state.is_mission_running:
-			    print("Mission ended prematurely - error.")
-			    exit(1)
-			if not good_frame and world_state.number_of_video_frames_since_last_state > 0:
-			    frame_x = world_state.video_frames[-1].xPos
-			    frame_z = world_state.video_frames[-1].zPos
-			    if math.fabs(frame_x - (x + 0.5)) < 0.001 and math.fabs(frame_z - (z-1)) < 0.001:
-			        good_frame = True
-			        end_frame = timer()
+	#Teleports the agent to location (x, 227, z) on a good_frame.
+	def teleport(self, agent_host, x, z, name):
+		#Construct and execute the teleport command.
+		tp_command = "chat /tp " + name + " " + str(x + 0.5) + " 227 " + str(z-1)
+		agent_host.sendCommand(tp_command)
 
 	def get_note_locations(self,agent_host, note_id):
 		return self.note_positions[note_id]
 
 	#Plays the noteblock specified by note_id.
-	def play_noteblock(self, agent_host, note_id):
+	def teleport_to_noteblock(self, agent_host, note_id, name):
 		#Obtain x,z coordinates of noteblock location based on note_id.
 		note_location_x, note_location_z = self.get_note_locations(agent_host, note_id)
 
 		#Perform teleport in front of stated noteblock.
-		self.teleport(agent_host, note_location_x, note_location_z)
+		self.teleport(agent_host, note_location_x, note_location_z, name)
+
+		#Set so we know to play a note.
+		self.can_play = True
 		time.sleep(0.1)
 
-		#Perform the hit. time.sleep() used to ensure expected behavior.
-		agent_host.sendCommand("attack 1")
-		time.sleep(0.001)
-		agent_host.sendCommand("attack 0")
-		time.sleep(1)
+		
--- code/simulation.py
+++ code/simulation.py
@@ -5,6 +5,7 @@
 # ------------------------------------------------------------------------------------------------
 
 import MalmoPython
+import malmoutils
 import sys
 import json
 from musician import *
@@ -89,7 +90,7 @@ def getMissionXML():
         <ServerSection>
             <ServerInitialConditions>
                 <Time>
-                    <StartTime>6000</StartTime>
+                    <StartTime>0</StartTime>
                     <AllowPassageOfTime>false</AllowPassageOfTime>
                 </Time>
                 <Weather>clear</Weather>
@@ -124,59 +125,140 @@ def getMissionXML():
             </AgentHandlers>
         </AgentSection>
 
+        <AgentSection mode="Survival">
+            <Name>Agent2</Name>
+            <AgentStart>
+                <Placement x="1.5" y="227.0" z="1.5" yaw="0" pitch="50"/>
+            </AgentStart>
+            <AgentHandlers>
+                <ContinuousMovementCommands turnSpeedDegs="480"/>
+                <AbsoluteMovementCommands/>
+                <SimpleCraftCommands/>
+                <MissionQuitCommands/>
+                <InventoryCommands/>
+                <ObservationFromNearbyEntities>
+                    <Range name="entities" xrange="40" yrange="40" zrange="40"/>
+                </ObservationFromNearbyEntities>
+                <ObservationFromFullInventory/>
+            </AgentHandlers>
+        </AgentSection>
+
     </Mission>'''
 
-#runs the simulation.
-def run(agent_host, musician):
-	for note_id in range(len(pitches)):
-		musician.play_noteblock(agent_host, note_id)
+def startMission(agent_host, mission, client_pool, recording, role, experimentId):
+    used_attempts = 0
+    max_attempts = 5
+    print("Calling startMission for role", role)
+    while True:
+        try:
+            agent_host.startMission(mission, client_pool, recording, role, experimentId)
+            break
+        except MalmoPython.MissionException as e:
+            errorCode = e.details.errorCode
+            if errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_WARMING_UP:
+                print("Server not quite ready yet - waiting...")
+                time.sleep(2)
+            elif errorCode == MalmoPython.MissionErrorCode.MISSION_INSUFFICIENT_CLIENTS_AVAILABLE:
+                print("Not enough available Minecraft instances running.")
+                used_attempts += 1
+                if used_attempts < max_attempts:
+                    print("Will wait in case they are starting up.", max_attempts - used_attempts, "attempts left.")
+                    time.sleep(2)
+            elif errorCode == MalmoPython.MissionErrorCode.MISSION_SERVER_NOT_FOUND:
+                print("Server not found - has the mission with role 0 been started yet?")
+                used_attempts += 1
+                if used_attempts < max_attempts:
+                    print("Will wait and retry.", max_attempts - used_attempts, "attempts left.")
+                    time.sleep(2)
+            else:
+                print("Other error:", e.message)
+                print("Waiting will not help here - bailing immediately.")
+                exit(1)
+        if used_attempts == max_attempts:
+            print("All chances used up - bailing now.")
+            exit(1)
+
+    print("startMission called okay.")
+
+def waitForStart(agent_hosts):
+    print("Waiting for the mission to start", end=' ')
+    start_flags = [False for a in agent_hosts]
+    start_time = time.time()
+    time_out = 120  # Allow two minutes for mission to start.
+    while not all(start_flags) and time.time() - start_time < time_out:
+        states = [a.peekWorldState() for a in agent_hosts]
+        start_flags = [w.has_mission_begun for w in states]
+        errors = [e for w in states for e in w.errors]
+        if len(errors) > 0:
+            print("Errors waiting for mission start:")
+            for e in errors:
+                print(e.text)
+            print("Bailing now.")
+            exit(1)
+        time.sleep(0.1)
+        print(".", end=' ')
+    print()
+    if time.time() - start_time >= time_out:
+        print("Timed out waiting for mission to begin. Bailing.")
+        exit(1)
+    print("Mission has started.")
 
 def main():
     print('Starting...', flush=True)
 
-    my_client_pool = MalmoPython.ClientPool()
-    my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
-
     agent_host = MalmoPython.AgentHost()
+    agent_host2 = MalmoPython.AgentHost()
 
-    try:
-    	agent_host.parse( sys.argv )
-    except RuntimeError as e:
-    	print('ERROR:', e)
-    	print(agent_host.getUsage())
-    	exit(1)
-    if agent_host.receivedArgument("help"):
-    	print(agent_host.getUsage())
-    	exit(0)
-
-    musician = Musician(note_positions)
+    malmoutils.parse_command_line(agent_host)
 
     my_mission = MalmoPython.MissionSpec(getMissionXML(), True)
-    my_mission_record = MalmoPython.MissionRecordSpec()
-    my_mission.requestVideo(800, 500)
-    my_mission.setViewpoint(0)
+    my_mission.allowAllChatCommands()
 
-    # Attempt to start a mission:
-    max_retries = 3
-    for retry in range(max_retries):
-    	try:
-    		agent_host.startMission(my_mission, my_client_pool, my_mission_record, 0, "Agent1")
-    		break
-    	except RuntimeError as e:
-    		if retry == max_retries - 1:
-    			print("Error starting mission:",e)
-    			exit(1)
-    		else:
-    			time.sleep(2)
+    my_client_pool = MalmoPython.ClientPool()
+    my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10000))
+    my_client_pool.add(MalmoPython.ClientInfo("127.0.0.1", 10001))
 
-    world_state = agent_host.getWorldState()	
-    while not world_state.has_mission_begun:
-        time.sleep(0.1)
-        world_state = agent_host.getWorldState()
-        time.sleep(1)
+    MalmoPython.setLogging("", MalmoPython.LoggingSeverityLevel.LOG_OFF)
+
+    musician = Musician(note_positions)
+    musician2 = Musician(note_positions)
+
+    startMission(agent_host, my_mission, my_client_pool, malmoutils.get_default_recording_object(agent_host, "agent_1_viewpoint_discrete"), 0, '' )
+    startMission(agent_host2, my_mission, my_client_pool, malmoutils.get_default_recording_object(agent_host, "agent_2_viewpoint_discrete"), 1, '' )
+    waitForStart([agent_host, agent_host2])
+
+    time.sleep(1)
 
     #Simulation starts here.
-    run(agent_host, musician)
+    agent_names = ["Agent1", "Agent2"]
+    i = 0
+    j = 24
 
+    #go through each note and hit it, Agent1 starts from left, Agent2 starts from right.
+    while i < 25:
+
+        #Call teleport on both agents and use time.sleep(1) to ensure everything is okay
+        musician.teleport_to_noteblock(agent_host, i, agent_names[0])
+        musician2.teleport_to_noteblock(agent_host, j, agent_names[1])
+        time.sleep(0.1)
+
+        #If the musician/agent received a note to play then hit it.
+        if musician.can_play:
+            agent_host.sendCommand("attack 1")
+            
+            time.sleep(0.001)
+            agent_host.sendCommand("attack 0")
+            musician.can_play = False
+
+        if musician2.can_play:
+            agent_host2.sendCommand("attack 1")
+            time.sleep(0.001)
+            agent_host2.sendCommand("attack 0")
+            musician2.can_play = False
+
+        time.sleep(1)
+        i+=1
+        j-=1
+    
 if __name__ == '__main__':
 	main()	
